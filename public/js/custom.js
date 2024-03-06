60021472b8c22b2f4c3f5a3a4c47289d349da3a3
Gid3onJura <homebasekorlash@googlemail.com>
2024-03-06 22:47:43 +0100

    reset code form

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,26 +1,3 @@
-;(function () {
-  "use strict"
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation")
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add("was-validated")
-      },
-      false
-    )
-  })
-})()
-
 $(document).ready(function () {
   // show toasts
   $(document).ready(function () {
@@ -28,11 +5,12 @@ $(document).ready(function () {
   })
 
   // validate registration form
-  validateForm("registrationForm")
+  validateRegistrationForm()
+  validateResetCodeForm()
 })
 
-function validateForm(formid) {
-  const form = document.querySelector("#" + formid)
+function validateRegistrationForm() {
+  const form = document.querySelector("#registrationForm")
   let response = null
 
   form.addEventListener(
@@ -43,39 +21,28 @@ function validateForm(formid) {
       if (!form.checkValidity()) {
       } else {
         // check data
-        switch (formid) {
-          case "registrationForm":
-            const serializedFormData = $(form).serialize()
-            $.post("/ajax/checkregistration", serializedFormData).done(function (response) {
-              if (response && !response.message) {
-                // submit form
-                $.post("/ajax/register", serializedFormData).done(function (response) {
-                  if (response && response.accessCode && response.nickname) {
-                    Swal.fire({
-                      title: "Registrierung erfolgreich!",
-                      html:
-                        "Mit folgendem Code <strong>" +
-                        response.accessCode +
-                        "</strong> kannst du dich einloggen! Merk ihn dir, sonst kannst du dich nicht einloggen oder an Gruppenspielen teilnehmen.",
-                      icon: "success",
-                      confirmButtonText: "Ok",
-                    }).then(function () {
-                      // switch to login tab
-                      const loginTab = $("#nav-login-tab")
-                      loginTab.trigger("click")
+        const serializedFormData = $(form).serialize()
+        $.post("/ajax/checkregistration", serializedFormData).done(function (response) {
+          if (response && !response.message) {
+            // submit form
+            $.post("/ajax/register", serializedFormData).done(function (response) {
+              if (response && response.accessCode && response.nickname) {
+                Swal.fire({
+                  title: "Registrierung erfolgreich!",
+                  html:
+                    "Mit folgendem Code <strong>" +
+                    response.accessCode +
+                    "</strong> kannst du dich einloggen! Merk ihn dir, sonst kannst du dich nicht einloggen oder an Gruppenspielen teilnehmen.",
+                  icon: "success",
+                  confirmButtonText: "Ok",
+                }).then(function () {
+                  // switch to login tab
+                  const loginTab = $("#nav-login-tab")
+                  loginTab.trigger("click")
 
-                      // set access code and nickname
-                      $("#login_code").val(response.accessCode)
-                      $("#login_nickname").val(response.nickname)
-                    })
-                  } else if (response.message) {
-                    Swal.fire({
-                      title: "Schade!",
-                      html: response.message,
-                      icon: "error",
-                      confirmButtonText: "Ok",
-                    })
-                  }
+                  // set access code and nickname
+                  $("#login_code").val(response.accessCode)
+                  $("#login_nickname").val(response.nickname)
                 })
               } else if (response.message) {
                 Swal.fire({
@@ -86,10 +53,42 @@ function validateForm(formid) {
                 })
               }
             })
-        }
+          } else if (response.message) {
+            Swal.fire({
+              title: "Schade!",
+              html: response.message,
+              icon: "error",
+              confirmButtonText: "Ok",
+            })
+          }
+        })
       }
 
       form.classList.add("was-validated")
+    },
+    false
+  )
+}
+
+function validateResetCodeForm() {
+  const form = document.querySelector("#resetCodeForm")
+  const inputEmail = $("#inputResetCodeEmail")
+  let response = null
+
+  form.addEventListener(
+    "submit",
+    function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      if (!validateEmail(inputEmail.val())) {
+        inputEmail.addClass("is-invalid")
+        form.classList.remove("was-validated")
+      } else {
+        inputEmail.removeClass("is-invalid")
+        form.classList.add("was-validated")
+        // check data
+        const serializedFormData = $(form).serialize()
+      }
     },
     false
   )
